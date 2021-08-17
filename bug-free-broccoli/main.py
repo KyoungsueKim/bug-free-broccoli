b34150c4979dc22436e75380b60b88c880aa1d5d
BuildTools <unconfigured@null.spigotmc.org>
2021-08-18 01:40:39 +0900

    Create feature_02 branch

--- bug-free-broccoli/main.py
+++ bug-free-broccoli/main.py
@@ -2,3 +2,4 @@ from packages import uData
 
 post02 = uData.init('https://www.ajou.ac.kr/kr/ajou/notice.do?mode=view&articleNo=112441&article.offset=0&articleLimit=10', title=True, text=True)
 print(post02.contents)
+print()
